ea255a367968257240123ece880cff8c74d76531
David Stein <stein.rs@gmail.com>
2022-02-17 11:40:32 -0300

    Execicio 02

--- Aula01/Ex02/src/ex02/Ex02.java
+++ Aula01/Ex02/src/ex02/Ex02.java
@@ -9,11 +9,17 @@ public class Ex02 {
 
         Livro livro = new Livro();
 
+        System.out.print("Informe o Título do livro:");
+        livro.setTitulo(scanner.nextLine());
+        System.out.print("Informe o nome do leitor:");
+        livro.setNomeLeitor(scanner.nextLine());
         System.out.print("Informe a QTD de páginas que tem o livro: ");
         livro.setQtdPagTotal(scanner.nextInt());
         System.out.print("Informe a QTD de páginas já lidas: ");
         livro.setQtdPagLidas(scanner.nextInt());
 
+        System.out.printf("Título: %s \n", livro.getTitulo());
+        System.out.printf("Leitor: %s \n", livro.getNomeLeitor());
         System.out.printf("Você já leu %.2f%% do livro. \n",livro.progresso());
     }
 
