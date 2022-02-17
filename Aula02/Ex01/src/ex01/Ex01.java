85530f457713f07023d9e156e74c06a871f95932
David Stein <stein.rs@gmail.com>
2022-02-17 20:38:01 -0300

    Exercicio 01

---
+++ Aula02/Ex01/src/ex01/Ex01.java
@@ -0,0 +1,14 @@
+package ex01;
+
+public class Ex01 {
+
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa("David Alexandre", "(41)98840335");   
+        
+        pessoa.setEndereço("rua julia skora, 185");
+       
+        System.out.printf("Nome: %s e telefone %s \n", pessoa.getNome(),pessoa.getTelefone());
+        System.out.println("endereço: " + pessoa.getEndereço());
+    }
+
+}
