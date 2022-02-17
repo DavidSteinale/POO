4143e9c4fda196d33d9ab4b879f66cb3f0be79d3
David Stein <stein.rs@gmail.com>
2022-02-17 11:33:48 -0300

    Execicio 02

--- Aula01/Ex02/src/ex02/Ex02.java
+++ Aula01/Ex02/src/ex02/Ex02.java
@@ -1,10 +1,20 @@
 package ex02;
 
+import java.util.Scanner;
+
 public class Ex02 {
 
     public static void main(String[] args) {
-        
-        
+        Scanner scanner = new Scanner(System.in);
+
+        Livro livro = new Livro();
+
+        System.out.print("Informe a QTD de páginas que tem o livro: ");
+        livro.setQtdPagTotal(scanner.nextInt());
+        System.out.print("Informe a QTD de páginas já lidas: ");
+        livro.setQtdPagLidas(scanner.nextInt());
+
+        System.out.printf("Você já leu %.2f%% do livro. \n",livro.progresso());
     }
 
 }
--- Aula01/Ex02/src/ex02/Livro.java
+++ Aula01/Ex02/src/ex02/Livro.java
@@ -6,4 +6,58 @@ public class Livro {
     private String nomeLeitor;
     private int qtdPagTotal;
     private int qtdPagLidas;
+    private float porcentagem;
+
+    
+    // CONSTRUTOR
+    public Livro() {
+        this.qtdPagTotal = 0;
+        this.qtdPagLidas = 0;
+    }
+    
+    //METODOS
+    
+    public float progresso(){
+        this.porcentagem = ((float)qtdPagLidas*100)/(float)qtdPagTotal;
+        return this.porcentagem;
+    }
+    
+    
+    // METODOS DE MODIFICAÇÂO
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getNomeLeitor() {
+        return nomeLeitor;
+    }
+
+    public void setNomeLeitor(String nomeLeitor) {
+        this.nomeLeitor = nomeLeitor;
+    }
+
+    public int getQtdPagTotal() {
+        return qtdPagTotal;
+    }
+
+    public void setQtdPagTotal(int qtdPagTotal) {
+        this.qtdPagTotal = qtdPagTotal;
+    }
+
+    public int getQtdPagLidas() {
+        return qtdPagLidas;
+    }
+
+    public void setQtdPagLidas(int qtdPagLidas) {
+        this.qtdPagLidas = qtdPagLidas;
+    }
+    
+    
+    
+    
 }
