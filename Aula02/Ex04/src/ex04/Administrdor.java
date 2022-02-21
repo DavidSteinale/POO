cf986dea057525de5c0373d9d15ca7d3f592d95f
David Stein <stein.rs@gmail.com>
2022-02-21 13:35:57 -0300

    exercicio 04

--- Aula02/Ex04/src/ex04/Administrdor.java
+++ Aula02/Ex04/src/ex04/Administrdor.java
@@ -1,13 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ex04;
 
-/**
- *
- * @author Stein
- */
-public class Administrdor {
+import java.math.BigDecimal;
+
+public class Administrdor extends Empregado{
     
+    private BigDecimal ajudaDeCusto;
+    
+    public Administrdor(String nome, String telefone) {
+        super(nome, telefone);
+    }
+    
+
 }
