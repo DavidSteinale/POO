5134b81cc2000f2676e37de7c3355e23bd0a1ebd
David Stein <stein.rs@gmail.com>
2022-02-21 13:30:33 -0300

    exercicio 04

---
+++ Aula02/Ex04/src/ex04/Empregado.java
@@ -0,0 +1,51 @@
+package ex03;
+
+import java.math.BigDecimal;
+
+public class Empregado extends Pessoa {
+    
+    private int codigoSetor;
+    private BigDecimal salarioBase;
+    private BigDecimal imposto;
+
+    //CONSTRUTOR HERDADO DA CLASSE PESSOA
+    public Empregado(String nome, String telefone) {
+        super(nome, telefone);
+    }
+
+    //METODO
+    public void calcularSalario() {
+        BigDecimal valor = this.getImposto().multiply(this.getSalarioBase());
+        
+        System.out.printf("Nome do funcionario: %s \n", this.getNome());
+        System.out.printf("Salario base: %.2f \n", this.getSalarioBase());
+        System.out.println("salario Liquido " + this.getSalarioBase().subtract(valor));
+        System.out.printf("Valor do imposto pago %.2f%% \n", this.getImposto());
+    }
+
+    //METODOS MODIFICADORES
+    public int getCodigoSetor() {
+        return codigoSetor;
+    }
+    
+    public void setCodigoSetor(int codigoSetor) {
+        this.codigoSetor = codigoSetor;
+    }
+    
+    public BigDecimal getSalarioBase() {
+        return salarioBase;
+    }
+    
+    public void setSalarioBase(BigDecimal salarioBase) {
+        this.salarioBase = salarioBase;
+    }
+    
+    public BigDecimal getImposto() {
+        return imposto;
+    }
+    
+    public void setImposto(BigDecimal imposto) {
+        this.imposto = imposto.divide(BigDecimal.valueOf(100));
+    }
+    
+}
