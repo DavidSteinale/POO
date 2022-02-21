95eb00b33e34310b499a129c814698feb9bb9de0
David Stein <stein.rs@gmail.com>
2022-02-21 14:02:18 -0300

    exercicio 04

--- Aula02/Ex04/src/ex04/Administrdor.java
+++ Aula02/Ex04/src/ex04/Administrdor.java
@@ -2,13 +2,29 @@ package ex04;
 
 import java.math.BigDecimal;
 
-public class Administrdor extends Empregado{
-    
+public class Administrdor extends Empregado {
+
     private BigDecimal ajudaDeCusto;
-    
+
     public Administrdor(String nome, String telefone) {
         super(nome, telefone);
     }
-    
+
+    @Override
+    public void calcularSalario() {
+        System.out.println();
+        System.out.printf("Nome do Administrador: %s \n", this.getNome());
+        BigDecimal valor = this.getAjudaDeCusto().add(this.getSalarioBase());
+        System.out.printf("Salário do Administrador %.2f \n", valor);
+
+    }
+
+    public BigDecimal getAjudaDeCusto() {
+        return ajudaDeCusto;
+    }
+
+    public void setAjudaDeCusto(BigDecimal ajudaDeCusto) {
+        this.ajudaDeCusto = ajudaDeCusto;
+    }
 
 }
--- Aula02/Ex04/src/ex04/Ex04.java
+++ Aula02/Ex04/src/ex04/Ex04.java
@@ -1,8 +1,24 @@
 package ex04;
 
+import java.math.BigDecimal;
+import java.util.Scanner;
+
 public class Ex04 {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        Administrdor adm = new Administrdor("Pedro Henrique", "(416325633)");
+        
+        System.out.print("Informe o salario base? ");
+        adm.setSalarioBase(BigDecimal.valueOf(scanner.nextDouble()));
+        
+        System.out.print("Informe o valor da ajuda de custo? ");
+        adm.setAjudaDeCusto(BigDecimal.valueOf(scanner.nextDouble()));
+             
+        adm.calcularSalario();
+
+      
     }
 
 }
