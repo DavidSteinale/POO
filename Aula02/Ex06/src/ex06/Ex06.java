93bcc2db3c00b3cf114dc481b8a271e2e63e3b20
David Stein <stein.rs@gmail.com>
2022-02-22 11:08:56 -0300

    exercicio 06

--- Aula02/Ex06/src/ex06/Ex06.java
+++ Aula02/Ex06/src/ex06/Ex06.java
@@ -1,8 +1,26 @@
 package ex06;
 
+import java.math.BigDecimal;
+import java.util.Scanner;
+
 public class Ex06 {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Vendedor vendedor = new Vendedor("Maria Paula", "(416325633)");
+
+        System.out.print("Informe o salario base: ");
+        vendedor.setSalarioBase(BigDecimal.valueOf(scanner.nextDouble()));
+
+        System.out.print("Informe o valor das vendas: ");
+        vendedor.setValorVendas(scanner.nextBigDecimal());
+
+        System.out.print("Informe o percentual da comissão: ");
+        vendedor.setComissao((scanner.nextBigDecimal()));
+
+        vendedor.calcularSalario();
+
     }
 
 }
