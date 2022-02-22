68f0d535f8dcb2e5d9312ae452541c9e808c01a8
David Stein <stein.rs@gmail.com>
2022-02-22 11:09:38 -0300

    exercicio 03

--- Aula02/Ex03/src/ex03/Ex03.java
+++ Aula02/Ex03/src/ex03/Ex03.java
@@ -8,7 +8,7 @@ public class Ex03 {
         Empregado empregado = new Empregado("joao", "(416325633)");
 
         empregado.setSalarioBase(BigDecimal.valueOf(4785));
-        empregado.setImposto(BigDecimal.valueOf(5));
+        empregado.setImposto(BigDecimal.valueOf(5)); 
         empregado.calcularSalario();
     }
 
