ea9ae2eb4a29d0608676045599bcdbe8acc13af6
David Stein <stein.rs@gmail.com>
2022-03-02 16:04:56 -0300

    CADASTRO DE FORNECEDOR NO MYSQL

--- cadastroMysql/CadastroFornecedor/src/DAO/FornecedorDAO.java
+++ cadastroMysql/CadastroFornecedor/src/DAO/FornecedorDAO.java
@@ -1,6 +1,6 @@
 /**
  * CLASSE ONDE FAZ A COMUNICAÇÃO COM A CLASSE CONEXAODAO, ONDE SE CONECTA COM O BANCO E ENVIA O COMANDO DE "INSERT",
- * 
+ *
  */
 package DAO;
 
@@ -10,28 +10,27 @@ import java.sql.PreparedStatement;
 import javax.swing.JOptionPane;
 
 public class FornecedorDAO {
-      
-    Connection conn;    
-    PreparedStatement pstm;   
+
+    Connection conn;    //DECLARAÇÃO DE VARIAVEL DO TIPO CONEXÃO
+    PreparedStatement pstm;   // DECLARADO UMA VARIAVEL DO TIPO PreparedStatement PARA PODER EXECUTAR OS COMANDOS EM SQL
 
     public void inserir(FornecedorDTO objfornecedordao) { // RECEBE TODOS OS OBJETOS DA DTO(FornecedorDTO)
-        String sql = "insert into fornecedor (nome, endereco) value(?,?)";
-        
+        String sql = "insert into fornecedor (nome, endereco) value(?,?)"; //CRIADO UMA VARIAVEL STRING, ONDE CONSTA O COMANDO DE INSERT NO BANCO FORNECEDOR
+
         conn = new ConexaoDAO().conectaDB();
-        
+
         try {
-            
-            pstm = conn.prepareStatement(sql);
-            pstm.setString(1, objfornecedordao.getRazaoSocial());
+
+            pstm = conn.prepareStatement(sql);// ENVIA PARA A COMANDO DA VARIAVEL "SQL"
+            pstm.setString(1, objfornecedordao.getRazaoSocial()); // INFORMA PARA O PSTM OS CAMPOS QUE SERÁ PEGO AS INFORMAÇÕES E A POSIÇÃO DE CADA CAMPO "?" 
             pstm.setString(2, objfornecedordao.getEndereco());
-            
-            pstm.execute();
-            pstm.close();
-            
+
+            pstm.execute(); // EXECUTA O PROCEDIMENTO
+            pstm.close(); // FECHA O ACESSO AO BANCO
+
         } catch (Exception erro) {
             JOptionPane.showMessageDialog(null, "FornecedorDAO" + erro);
         }
-        
 
     }
 
